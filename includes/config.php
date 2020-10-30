8d94391e812dd5c387b889783e82472f3633c97a
Ivan <komar.ivan.97@gmail.com>
2020-10-30 02:06:33 +0200

    Added table

---
+++ includes/config.php
@@ -0,0 +1,11 @@
+<?php
+$config = array(
+    'db' => array(
+        'server' => 'localhost',
+        'username' => 'root',
+        'password' => 'root',
+        'name' => 'test_panel'
+    )
+);
+require "db.php";
+?> 
